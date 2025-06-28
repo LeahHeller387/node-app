4d16ec56c49f12c87a7df0361c35e0c4d12aeca8
shmic237 <xd.mshlezinger@idf.il>
2025-06-29 01:46:42 +0300

    end progect

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 connectToMongo().then(() => {
   app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
   });
 }).catch((err) => {
   console.error('Failed to connect to MongoDB:', err);
